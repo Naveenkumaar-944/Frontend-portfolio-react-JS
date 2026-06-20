5902e083fda59682179bd2db02e258ad9f77d05c
Naveenkumaar-944 <naveenkumaarnk2@gmail.com>
2026-06-20 20:44:18 +0530

    Initial portfolio upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./Frontend-portfolio-react-JS"
+  base: '/Frontend-portfolio-react-JS/'
 })
